900401c32e34bdc50741f37eacef38c49128bc66
JCChiou <rex2763@gmail.com>
2023-03-11 12:11:46 +0800

    a template of error handler

--- routers/login.js
+++ routers/login.js
@@ -20,40 +20,15 @@ router.get("/", async (ctx) => {
 });
 
 
-// router.post("/login", { validate: { type: "json" } }, testlog, async (ctx) => {
-//   const body = ctx.request.body;
-//   // try {
-//     if (body.userid == "user" && body.userpwd == "1234") {
-//       ctx.status = 200;
-//       ctx.body = true;
-//     } else {
-//       // ctx.status = 500;
-//       // ctx.body = "error body"
-//       throw new Error("throw error");
-//     }
-//   // } catch (err) {
-//     // console.log('xxxxxxxxx')
-//     // ctx.status = 500;
-//     // ctx.body = { message: "錯誤訊息" };
-//   // }
-// });
-
 router.post("/login", testlog, async (ctx) => {
   const body = ctx.request.body;
-  // try {
     if (body.userid == "user" && body.userpwd == "1234") {
       ctx.status = 200;
       ctx.body = true;
     } else {
-      // ctx.status = 500;
-      // ctx.body = "error body"
       throw new Error("throw error");
     }
-  // } catch (err) {
-    // console.log('xxxxxxxxx')
-    // ctx.status = 500;
-    // ctx.body = { message: "錯誤訊息" };
-  // }
+ 
 });
 
 export default router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import routing from "./routers/index.js";
 import logger from "koa-logger";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { NotFoundError } from "./middleware/errorHandler.js";
+import { json } from "stream/consumers";
 
 const app = new Koa();
 
@@ -16,45 +17,23 @@ const errorMiddleware = async (ctx, next) => {
   } catch (error) {
     console.error(error);
     if (error instanceof NotFoundError) {
-      ctx.throw(404, 'Not Found', {message: "this is 404"})
+      ctx.throw(404, {message: "this is 404 error"})
+    }else {
+      ctx.throw(401,{message: "login fail"})
     }
+   
   }
 }
 
 app.use(errorMiddleware);
 
-// app.use(async (ctx, next) => {
-//   try {
-//     console.log('before next')
-//     await next();
-//     console.log('after next , pass first try catch');
-//     // await Promise.reject(new Error("something error"));
-//   } catch (err) {
-//     console.log("攔截錯誤訊息：", err.message);
-//     // ctx.app.emit("error", err, ctx)
-//     // ctx.throw(500, err.message)
-   
-//     ctx.status =  500;
-//     ctx.body = {
-//       message: err.message
-//     };
-//     ctx.throw(400, " error hello world")
-//   }
-// });
-
-// app.use((ctx, next) => {
-//   ctx.throw(400,"123")
-//   ctx.status = 200
-//   console.log('Setting status')
-//   // need to return here, not using async-await
-//   return next()
-// })
 
 routing(app);
 
 
-app.on("error", async (err, ctx) => {
+app.on("error", async (err, ctx, res) => {
   console.error("hihi app on error",err.message);
+  console.log('ctx msg =', ctx.response)
   // console.error("app on error", err, ctx);
 });
 
